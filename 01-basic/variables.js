4ee38a55e66c0876b502578b4a05815c1656249f
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-05-09 14:57:24 +0530

    Add files via upload

---
+++ 01-basic/variables.js
@@ -0,0 +1,11 @@
+const accountName = "shaikh abdul rakheeb";
+let accountBalance = 1000;
+var accountNumber = 92876736;
+// var did not used this time only let and const are used
+
+console.log(accountName);
+console.table([accountBalance, accountNumber]);
+
+let college = "pes college of engineering aurangabad"
+console.log(college);
+
