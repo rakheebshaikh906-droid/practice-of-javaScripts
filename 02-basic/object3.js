ce04dcef96a720052b3ca60fb62101e8498108df
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-05-12 14:26:00 +0530

    Add files via upload

---
+++ 02-basic/object3.js
@@ -0,0 +1,17 @@
+
+//de-structuring
+let person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+//de-structuring assignment
+let { name, age, city } = person;
+console.log(name); // "John"
+console.log(age); // 30
+console.log(city); // "New York"
+//de-structuring with arrays
+let arr = [1, 2, 3, 4, 5];
+let [a, b, c] = arr;
+console.log(a);
+
