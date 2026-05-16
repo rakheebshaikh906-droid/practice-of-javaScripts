53c0d012348790b2b0b7865a96085516b49b01f1
shaikh abdul rakheeb <rakheebshaikh906@gmail.com>
2026-05-16 15:01:16 +0530

    Add files via upload

---
+++ leetcode-Question/2703.js
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/return-length-of-arguments-passed/description/
+//2703. Return Length of Arguments Passed
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+
+console.log(argumentsLength(1, 2, 3));
